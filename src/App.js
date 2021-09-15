8c943c254ba91521b5d4ff9ff936a7a28af305a0
Oybek Narzullayev <oybeknarzullaev@gmail.com>
2021-09-15 19:01:28 +0500

    login register responsible pages

--- src/App.js
+++ src/App.js
@@ -4,16 +4,28 @@ import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
 import TopBar from "./components/topbar/TopBar";
 import Settings from "./pages/settings/Settings";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import { Link, Route, Switch, BrowserRouter as Router } from "react-router-dom";
 
 function App() {
+  const user = false;
   return (
-    <>
+    <Router>
       <TopBar />
-      <Settings />
-      {/* <Write /> */}
-      {/* <Single /> */}
-      {/* <Home /> */}
-    </>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/register">{user ? <Home /> : <Register />}</Route>
+        <Route path="/login">{user ? <Home /> : <Login />}</Route>
+        <Route path="/setings">{user ? <Settings /> : <Register />}</Route>
+        <Route path="/write">{user ? <Write /> : <Register />}</Route>
+        <Route path="post/:postId">
+          <Single />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -1,6 +1,8 @@
+import { Link } from "react-router-dom";
 import "./topbar.css";
 
 function TopBar() {
+  const user = false;
   return (
     <div className="top">
       <div className="topLeft">
@@ -11,19 +13,58 @@ function TopBar() {
       </div>
       <div className="topCenter">
         <ul className="topList">
-          <li className="topListItem">HOME</li>
-          <li className="topListItem">ABOUT</li>
-          <li className="topListItem">CONTACT</li>
-          <li className="topListItem">WRITE</li>
-          <li className="topListItem">LOGOUT</li>
+          <li className="topListItem">
+            <Link className="link" to="/">
+              HOME
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/">
+              ABOUT
+            </Link>
+          </li>
+          <li className="topListItem">
+            {" "}
+            <Link className="link" to="/">
+              CONTACT
+            </Link>
+          </li>
+          <li className="topListItem">
+            {" "}
+            <Link className="link" to="/write">
+              WRITE
+            </Link>
+          </li>
+          <li className="topListItem">
+            {" "}
+            <Link className="link" to="/">
+              {user && "LOGOUT"}
+            </Link>
+          </li>
         </ul>
       </div>
       <div className="topRight">
-        <img
-          className="topImg"
-          src="https://png.pngtree.com/element_our/20190604/ourlarge/pngtree-user-avatar-boy-image_1482937.jpg"
-          alt="profile pic"
-        />
+        {user ? (
+          <img
+            className="topImg"
+            src="https://png.pngtree.com/element_our/20190604/ourlarge/pngtree-user-avatar-boy-image_1482937.jpg"
+            alt="profile pic"
+          />
+        ) : (
+          <ul className="topList">
+            <li className="topListItem">
+              <Link className="link" to="/login">
+                LOGIN
+              </Link>
+            </li>
+            <li className="topListItem">
+              <Link className="link" to="/register">
+                REGISTER
+              </Link>
+            </li>
+          </ul>
+        )}
+
         <i className="topSearchIcon fas fa-search"></i>
       </div>
     </div>
